269cb8ee71ea75905ca9d8b830e8501f07e7a37e
Ilya <IlyaDuginov1997@gmail.com>
2023-04-13 00:08:18 +0300

    feat: finish on delete-digit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,9 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let sameCharacters = 0;
+
+  const createObj = (str) => {
+    const obj = {};
+
+    for (i of str) {
+      if (obj.hasOwnProperty(i)) {
+        obj[i]++
+      } else {
+        obj[i] = 1;
+      }
+    }
+
+    return obj;
+  }
+
+  const firstStringObj = createObj(s1);
+  const secondStringObj = createObj(s2);
+
+  for (i in firstStringObj) {
+    if (!secondStringObj.hasOwnProperty(i)) continue
+    sameCharacters = sameCharacters + Math.min(firstStringObj[i], secondStringObj[i]);
+  }
+
+  return sameCharacters;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const origArray = String(n).split('').map(Number);
+  let arr = [];
+
+  for (let i = 0; i < origArray.length; i++) {
+    const copyArr = [...origArray];
+    const a = copyArr.splice(i, 1);
+    console.log(a)
+    arr = [...arr, copyArr.join('')];
+  }
+
+  return Math.max(...arr);
 }
 
 module.exports = {
