a4dadcdecc82edbce6bfe88921a380ffc535d4e6
Ilya Duginov <ilyaduginov1997@gmail.com>
2023-04-17 19:40:40 +0300

    feat: end what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -9,9 +9,39 @@
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date = 'no-value') {
+  if (date === 'no-value') return 'Unable to determine the time of year!';
 
+  if (date instanceof Date) {
+    if (Object.prototype.toString.call(date) !== '[object Date]') {
+
+      throw new Error('Invalid date!');
+    }
+
+    const currentMonth = date.getMonth();
+
+    switch (currentMonth) {
+      case 0:
+      case 1:
+      case 11:
+        return 'winter'
+      case 2:
+      case 3:
+      case 4:
+        return 'spring'
+      case 5:
+      case 6:
+      case 7:
+        return 'summer'
+      case 8:
+      case 9:
+      case 10:
+        return 'autumn'
+    }
+  }
 }
+
+console.log(getSeason('foo'))
 
 module.exports = {
   getSeason
